178cfd71edac06c77338105b5a6bf224adf8d944
Fady Derias <fadyderias_92@yahoo.com>
2017-03-29 10:36:24 +0200

    FEAT: Adding a Dictionary extension that does setup HTTP parameters in the appropriate form to handle a request.

---
+++ FlickrViewer/Categories/Dictionary+HTTPParameters.swift
@@ -0,0 +1,21 @@
+//
+//  Dictionary+HTTPParameters.swift
+//  FlickrViewer
+//
+//  Created by Fady on 3/29/17.
+//  Copyright © 2017 orange. All rights reserved.
+//
+
+import Foundation
+extension Dictionary {
+    
+    func stringFromHttpParameters() -> String {
+        let parameterArray = self.map { (key, value) -> String in
+            let percentEscapedKey = (key as! String).addingPercentEncodingForURLQueryValue()!
+            let percentEscapedValue = (value as! String).addingPercentEncodingForURLQueryValue()!
+            return "\(percentEscapedKey)=\(percentEscapedValue)"
+        }
+        
+        return parameterArray.joined(separator: "&")
+    }
+}
