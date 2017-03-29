f56bd34f4b5f238476b9b42b87c18ee32a2c793e
Fady Derias <fadyderias_92@yahoo.com>
2017-03-30 01:40:37 +0200

    FEAT: Initial setup for HomeSearchTableViewController..

--- FlickrViewer/UI/View Controllers/HomeSearchTableViewController.swift
+++ FlickrViewer/UI/View Controllers/HomeSearchTableViewController.swift
@@ -8,90 +8,44 @@
 
 import UIKit
 
-class HomeSearchTableViewController: UITableViewController {
+class HomeSearchTableViewController: UITableViewController, UISearchBarDelegate {
 
+    let searchTableViewCellIdentifier = "SearchTableViewCellIdentifier"
+    var searchPhotosResults: [FLPhoto]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let photoManager = FLPhotosManager()
-        
         photoManager.fetchPhotosBySearch(page: 1, userText: "Flower", perPage: 20, success: { (photosResult) in
-            print(photosResult.photosList?.photos?.count ?? 500)
+            self.searchPhotosResults = photosResult.photosList?.photos
+            OperationQueue.main.addOperation({
+                self.tableView.reloadData()
+            })
         }) { (error) in
             print(error)
         }
       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        guard let numberOfRows = searchPhotosResults?.count else {
+            return 0
+        }
+        
+        return numberOfRows
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: searchTableViewCellIdentifier) as! SearchTableViewCell
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
