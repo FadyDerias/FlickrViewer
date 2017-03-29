cad142f0bd58b2ad8f29f68f72fbb3dbb608774a
Fady Derias <fadyderias_92@yahoo.com>
2017-03-29 12:10:42 +0200

    FEAT: Adding FLPhoto model that implements Mappable (ObjectMapper) that parses JSON data to its parameters.

---
+++ FlickrViewer/Networking/Models/FLPhoto.swift
@@ -0,0 +1,32 @@
+//
+//  FLPhoto.swift
+//  FlickrViewer
+//
+//  Created by Fady on 3/29/17.
+//  Copyright © 2017 orange. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class FLPhoto: Mappable {
+    
+    var photoId: String?
+    var ownerId: String?
+    var secretId: String?
+    var serverId: NSString?
+    var farmId: Int?
+    
+    required init?(map: Map) {
+    }
+    
+    func mapping(map: Map) {
+        
+        photoId <- map["id"]
+        ownerId <- map["owner"]
+        secretId <- map["secret"]
+        serverId <- map["server"]
+        farmId <- map["farm"]
+
+    }
+}
