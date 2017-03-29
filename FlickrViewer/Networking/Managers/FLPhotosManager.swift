a0788f5f345d3c87a9d64b533b4b94518a5a58aa
Fady Derias <fadyderias_92@yahoo.com>
2017-03-29 11:30:12 +0200

    FEAT: Adding function to setup the URL to be utilized to fetch photos based on user input text.

--- FlickrViewer/Networking/Managers/FLPhotosManager.swift
+++ FlickrViewer/Networking/Managers/FLPhotosManager.swift
@@ -13,5 +13,22 @@ class FLPhotosManager {
     let apikey = "8bd0e104fbbbfe0a6d1b6a557f1f4365"
     let appSecret = "9a067a565d6244e4"
     
+    func setupURLForPhotosSearchByText(text: String, perPage: Int, page: Int) -> URL {
+
+        let searchBaseURL = "https://api.flickr.com/services/rest/"
+        let parametersDictionary = ["method":"flickr.photos.search",
+                                    "api_key":"8bd0e104fbbbfe0a6d1b6a557f1f4365",
+                                    "text":"\(text)",
+                                    "per_page":"\(perPage)",
+                                    "page": "\(page)",
+                                    "format":"json",
+                                    "nojsoncallback":"1"] as [String : Any]
+        
+        let httpParameters = parametersDictionary.stringFromHttpParameters()
+        let urlString = searchBaseURL.appending(httpParameters)
+        
+        return URL(string: urlString)!
+        
+    }
     
 }
