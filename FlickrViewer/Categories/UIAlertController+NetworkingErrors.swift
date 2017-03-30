c1de3babc0351c93fdfb6af6ece6b96efdd04f89
Fady Derias <fadyderias_92@yahoo.com>
2017-03-30 11:03:56 +0200

    FEAT: Adding UIAlertController category that does handle the following networking errors: -Internet connection: informing the user that there’s a problem with their internet connection. -Unavailable search results: informing the user that his/her input text did return 0 results.

---
+++ FlickrViewer/Categories/UIAlertController+NetworkingErrors.swift
@@ -0,0 +1,41 @@
+//
+//  UIAlertController+NetworkingErrors.swift
+//  FlickrViewer
+//
+//  Created by Fady on 3/30/17.
+//  Copyright © 2017 orange. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIAlertController {
+    
+    static func defaultNetworkingAlertController(_ retryHandler: @escaping (() -> Void)) -> UIAlertController {
+        let alertController = UIAlertController(title: "Error",
+                                                message: "Could not connect to the internet. Would you like to try again?",
+                                                preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        let retryAction = UIAlertAction(title: "Retry", style: .default) { (alertController) in
+            retryHandler()
+        }
+        
+        alertController.addAction(retryAction)
+        alertController.addAction(cancelAction)
+        
+        return alertController
+    }
+    
+    
+    
+    static func searchResultsAlertController() -> UIAlertController {
+        let alertController = UIAlertController(title: "Search Results",
+                                                message: "Sorry, no search results are available for this input search text.",
+                                                preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        return alertController
+        
+    }
+}
