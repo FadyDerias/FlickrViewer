44a6a080e157d0820ddebf78c1e6315e49d7fbfb
Fady Derias <fadyderias_92@yahoo.com>
2017-03-30 18:04:13 +0200

    FEAT: Downloading FLPhotos images and setting them to FlickrTableViewCell equivalent image view.

--- FlickrViewer/UI/View Configurations/FlickrPhotoTableViewCell+ConfigureForFLPhoto.swift
+++ FlickrViewer/UI/View Configurations/FlickrPhotoTableViewCell+ConfigureForFLPhoto.swift
@@ -13,5 +13,14 @@ extension FlickrPhotoTableViewCell {
     
     func configureForFlickrPhotoData(flPhoto: FLPhoto) {
         self.photoTitleLabel.text = flPhoto.title
+        
+        if let imageUrl = flPhoto.urlForFLPhotoImage() {
+            let flPhotoDownloader = FLPhotoDownloader()
+            flPhotoDownloader.downloadImage(url: imageUrl, success: { (data) in
+                OperationQueue.main.addOperation({
+                    self.photoImageView.image = UIImage(data: data)
+                })
+            })
+        }
     }
 }
