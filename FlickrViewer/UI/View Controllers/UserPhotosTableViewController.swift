60f6caccbef74e1da69847dbaf0754d398313a22
Fady Derias <fadyderias_92@yahoo.com>
2017-03-31 18:23:58 +0200

    FEAT: Setup of UsersTableViewController: -Loading photos of user by searching Flickr API with the user_id -Saving retrieved FLPhotos in an NSMutableArray data source and reloading the table view when ready. -Loading new photos when user scrolls to last but one cell (incrementing the page number query parameter in the last but one index)

--- FlickrViewer/UI/View Controllers/UserPhotosTableViewController.swift
+++ FlickrViewer/UI/View Controllers/UserPhotosTableViewController.swift
@@ -9,10 +9,79 @@
 import UIKit
 
 class UserPhotosTableViewController: UITableViewController {
+    
+    var searchPhotosResults = NSMutableArray()
+    let userPhotoTableViewCellIdentifier = "UserPhotoTableViewCellIdentifier"
+    var userId: String?
+    var nextPageToLoad: Int = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tableView.tableFooterView = UIView()
+        loadResultsForUserId()
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return searchPhotosResults.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: userPhotoTableViewCellIdentifier, for: indexPath) as!UserPhotoTableViewCell
+        
+        cell.accessoryType = .disclosureIndicator
+        let flPhoto = searchPhotosResults.object(at: indexPath.row) as! FLPhoto
+        cell.configureForFlickrPhotoData(flPhoto: flPhoto)
+        
+        return cell
+    }
+    
+    // MARK: UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if (indexPath.row == (searchPhotosResults.count - 1)) {
+            nextPageToLoad += 1
+            loadResultsForUserId()
+        }
+    }
+    
+    // MARK: NetworkRequest
+    
+    func loadResultsForUserId() {
+        let photoManager = FLPhotosManager()
+        if let ownerId = self.userId {
+            photoManager.fetchPhotosBySearch(page: nextPageToLoad, userText: nil, userId: ownerId, success: { (photosResult) in
+                
+                if let photos = photosResult.photosList?.photos {
+                    self.searchPhotosResults.addObjects(from: photos)
+                }
+                
+                OperationQueue.main.addOperation({
+                    self.tableView.reloadData()
+                    
+                    if (self.searchPhotosResults.count == 0)
+                    {
+                        let noResultsAlertController = UIAlertController.searchResultsAlertController()
+                        self.present(noResultsAlertController, animated: true, completion: nil)
+                    }
+                })
+            }) { (error) in
+                print(error)
+                let noInternetConnectionAlertController = UIAlertController.defaultNetworkingAlertController({
+                    self.loadResultsForUserId()
+                })
+                
+                OperationQueue.main.addOperation({
+                    self.present(noInternetConnectionAlertController, animated: true, completion: nil)
+                })
+            }
+        }
     }
 
 }
