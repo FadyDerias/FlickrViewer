5082dd673c9094f72ad411f9e84a9b28d768614f
Fady Derias <fadyderias_92@yahoo.com>
2017-03-31 15:47:21 +0200

    FEAT: Adding UserPhotosTableViewController in Main.storyboard + Creating a new show type segue from HomeSearchTableViewController for its display.

---
+++ FlickrViewer/UI/View Controllers/UserPhotosTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UserPhotosTableViewController.swift
+//  FlickrViewer
+//
+//  Created by Fady on 3/31/17.
+//  Copyright © 2017 orange. All rights reserved.
+//
+
+import UIKit
+
+class UserPhotosTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
