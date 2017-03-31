b245e3b044c247a996e2b747f386dba68388748d
Fady Derias <fadyderias_92@yahoo.com>
2017-03-31 16:12:56 +0200

    FEAT: Creating UserPhotosTableViewCell and Creating outlets for its subviews from Main.storyboard and setting their initial setup and constraints.

---
+++ FlickrViewer/UI/Views/UserPhotosTableViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  UserPhotosTableViewCell.swift
+//  FlickrViewer
+//
+//  Created by Fady on 3/31/17.
+//  Copyright © 2017 orange. All rights reserved.
+//
+
+import UIKit
+
+class UserPhotosTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupPhotoTitleLabel()
+        setupPhotoImageView()
+        setNeedsUpdateConstraints()
+    }
+    
+    func setupPhotoTitleLabel() {
+        self.photoTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.photoTitleLabel.numberOfLines = 0
+        self.photoTitleLabel.adjustsFontSizeToFitWidth = true
+    }
+    
+    func setupPhotoImageView() {
+        self.photoImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.photoImageView.contentMode = .scaleAspectFit
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.photoImageView.image = nil
+        self.photoTitleLabel.text = ""
+    }
+    
+    override func updateConstraints() {
+        super.updateConstraints()
+        
+        NSLayoutConstraint.activate([
+            self.photoImageView.leftAnchor.constraint(equalTo: self.contentView.leftAnchor, constant: 16),
+            self.photoImageView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+            self.photoImageView.widthAnchor.constraint(equalToConstant: 60),
+            self.photoImageView.heightAnchor.constraint(equalToConstant: 60),
+            
+            self.photoTitleLabel.leftAnchor.constraint(equalTo: self.photoImageView.rightAnchor, constant: 16),
+            self.photoTitleLabel.rightAnchor.constraint(equalTo: self.contentView.rightAnchor, constant: -16),
+            self.photoTitleLabel.topAnchor.constraint(equalTo: self.photoImageView.topAnchor),
+            self.photoTitleLabel.bottomAnchor.constraint(equalTo: self.photoImageView.bottomAnchor)
+            ])
+    }
+
+}
