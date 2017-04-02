7ee38392578097732678772f6eacb2fc6475a453
Fady Derias <fadyderias_92@yahoo.com>
2017-04-02 12:47:36 +0200

    FEAT: adding method in FLCoreDataManager that does save an FLPhotoObject parameters to core data.

--- FlickrViewer/Data Presistence/Core Data/Managers/FLCoreDataManager.swift
+++ FlickrViewer/Data Presistence/Core Data/Managers/FLCoreDataManager.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import CoreData
+
+class FLCoreDataManager {
+    
+    static let sharedInstance = FLCoreDataManager()
+    private var context = FLCoreDataStack.sharedInstance.managedObjectContext
+    
+ 
+    func savePhotoToCoreData(flickrPhotoEntity: NSEntityDescription?, flPhoto: FLPhoto) {
+    
+        if let entity = flickrPhotoEntity {
+            let flickrPhotoManagedObject = NSManagedObject(entity: entity,
+                                                           insertInto: context)
+            
+            flickrPhotoManagedObject.setValue(flPhoto.farmId, forKeyPath: "farmId")
+            flickrPhotoManagedObject.setValue(flPhoto.photoId, forKeyPath: "photoId")
+            flickrPhotoManagedObject.setValue(flPhoto.ownerId, forKeyPath: "ownerId")
+            flickrPhotoManagedObject.setValue(flPhoto.secretId, forKeyPath: "secretId")
+            flickrPhotoManagedObject.setValue(flPhoto.serverId, forKeyPath: "serverId")
+            flickrPhotoManagedObject.setValue(flPhoto.title, forKeyPath: "title")
+            
+            do {
+                try context.save()
+            } catch let error as NSError {
+                print("Could not save. \(error), \(error.userInfo)")
+            }
+        }
+        
+    }
+    
