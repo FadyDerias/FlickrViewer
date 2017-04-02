43f97eefe8fba7f0112a1c5316f931894a59b06d
Fady Derias <fadyderias_92@yahoo.com>
2017-04-02 12:44:46 +0200

    FEAT: Adding FLCoreDataManager and FLUserDefaultsManager.

---
+++ FlickrViewer/Data Presistence/Core Data/Managers/FLCoreDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  FLCoreDataManager.swift
+//  FlickrViewer
+//
+//  Created by Fady on 4/2/17.
+//  Copyright © 2017 orange. All rights reserved.
+//
+
+import Foundation
---
+++ FlickrViewer/Data Presistence/User Defaults/FLUserDefaultsManager.swift
@@ -0,0 +1,9 @@
+//
+//  FLUserDefaultsManager.swift
+//  FlickrViewer
+//
+//  Created by Fady on 4/2/17.
+//  Copyright © 2017 orange. All rights reserved.
+//
+
+import Foundation
