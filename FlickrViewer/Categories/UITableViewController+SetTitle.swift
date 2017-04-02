cb047397b9520e6c3417454370e166fd6178447f
Fady Derias <fadyderias_92@yahoo.com>
2017-04-03 01:24:53 +0200

    FEAT: Adding extension for UITableViewController that does a table view controller title (on the navigation bar).

---
+++ FlickrViewer/Categories/UITableViewController+SetTitle.swift
@@ -0,0 +1,17 @@
+//
+//  UITableViewController+SetTitle.swift
+//  FlickrViewer
+//
+//  Created by Fady on 4/3/17.
+//  Copyright © 2017 orange. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITableViewController {
+    
+    func setNavigationBarTitle(title: String) {
+        self.title = title
+    }
+}
