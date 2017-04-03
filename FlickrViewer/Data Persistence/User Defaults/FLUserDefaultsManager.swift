5ad3aa08643a2465ebe98dc71c729b826cc1bece
Fady Derias <fadyderias_92@yahoo.com>
2017-04-03 13:17:46 +0200

    FEAT: Modifying FLUserDefaultsManager to save/load user’s previous search text in user defaults.

--- FlickrViewer/Data Persistence/User Defaults/FLUserDefaultsManager.swift
+++ FlickrViewer/Data Persistence/User Defaults/FLUserDefaultsManager.swift
@@ -13,21 +13,27 @@ class FLUserDefaultsManager {
     static let sharedInstance = FLUserDefaultsManager()
     private let defaults: UserDefaults = UserDefaults.standard
     let pageToLoadKey = "nextPageToLoad"
-    let lastUserSearchTextLey = "searchText"
+    let userSearchTextKey = "searchText"
+    let userPreviousSearchTextKey = "previousSearchText"
     
-    func saveSearchParametersInUserDefaults(nextPageToLoad: Int,userInputSearchText: String ) {
+    func saveSearchParametersInUserDefaults(nextPageToLoad: Int,userInputSearchText: String, userPreviousSearchText: String) {
         
         defaults.set(nextPageToLoad, forKey: pageToLoadKey)
-        defaults.set(userInputSearchText, forKey: lastUserSearchTextLey)
+        defaults.set(userInputSearchText, forKey: userSearchTextKey)
+        defaults.set(userInputSearchText, forKey: userSearchTextKey)
+        defaults.set(userPreviousSearchText, forKey: userPreviousSearchTextKey)
+
         defaults.synchronize()
     }
     
     func loadSearchParametersFromUserDefaults() -> NSDictionary? {
         let nextPageToLoad = defaults.value(forKey: pageToLoadKey) as? Int
-        let searchText = defaults.value(forKey: lastUserSearchTextLey) as? String
+        let searchText = defaults.value(forKey: userSearchTextKey) as? String
+        let previousSearchText = defaults.value(forKey: userPreviousSearchTextKey) as? String
         
         if let userSearchText = searchText, let pageToLoadNumber = nextPageToLoad {
-            let searchParametersDictionary = [lastUserSearchTextLey:userSearchText,
+            let searchParametersDictionary = [userSearchTextKey:userSearchText,
+                                              userPreviousSearchTextKey:previousSearchText as Any,
                                               pageToLoadKey:pageToLoadNumber] as [String : Any]
             
             return searchParametersDictionary as NSDictionary
